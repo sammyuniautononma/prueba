3d365b8ef8f2e9827b2419962d3b6669728495e7
sammyuniautononma <117314063+sammyuniautononma@users.noreply.github.com>
2022-11-02 15:26:03 -0500

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,24 @@
+
+<?php
+   $diccionario = [
+     1 => 0,
+     2 => 0,
+     3 => 0,
+     4 => 0,
+     5 => 0,
+     ];
+
+   $myArray = [1, 2, 1, 3, 3, 1, 2, 1, 5, 1];
+     foreach ($muArray as $numero) 
+     {$diccionario[$nuevo]++;
+      }
+
+     foreach ($diccionario as $clave => $conteo)
+     	{echo "$clave: ";
+     	 for ($x = 0; $x < $conteo; $x++)
+     	   echo "*";
+     	}
+
+     echo "\n";
+?>
+
